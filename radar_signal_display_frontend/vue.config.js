f8bcba9fd863704b405ff52e8fce75cc9a44d187
sxy <604795206@qq.com>
2025-07-03 17:23:12 +0800

    env 调整

--- radar_signal_display_frontend/vue.config.js
+++ radar_signal_display_frontend/vue.config.js
@@ -1,4 +1,12 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: 'warning',
+  chainWebpack: config => {
+    config.resolve.alias.set('@', resolve('src'))
+  }
 })
